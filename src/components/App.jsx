501299ca0408b5c94e226769890bb5d2e0135aa8
Ivan Baklan <51780841+ivbaklan2012@users.noreply.github.com>
2022-05-11 19:01:38 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
+
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
